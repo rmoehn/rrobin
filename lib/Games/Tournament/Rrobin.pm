7c7f92a9cd28cdbe4db4ea40d94a21666f957f74
Richard Möhn <myrtscht@cpan.org>
2010-07-12 17:53:12 +0200

    add logging and command line options
    Rrobin now supports logging via Log::Log4perl. The added command line
    options help the test suite and possible developers.

--- lib/Games/Tournament/Rrobin.pm
+++ lib/Games/Tournament/Rrobin.pm
@@ -30,12 +30,24 @@ use 5.010;
 use warnings;
 use strict;
 
+use Log::Log4perl ':easy';
+use Carp;
 use Gtk2 '-init';
 use Gtk2::GladeXML::Simple;
 use base 'Gtk2::GladeXML::Simple';
 
+
 our $VERSION = '0.001';
 $VERSION = eval $VERSION;
+
+
+# For documentation see the OTHER SUBROUTINES section some way down this
+# document.
+sub _log_call () { 
+    local $Log::Log4perl::caller_depth += 1;
+
+    DEBUG 'Called.';
+}
 
 
 =head1 METHODS
@@ -55,10 +67,12 @@ sub new {
 
     # First we have to find the .glade file.
     my $module_path = __FILE__;
-    (my $glade_file = $module_path) =~ s/Rrobin\.pm/rrobin.glade/;
+    (my $glade_file = $module_path) =~ s/Rrobin\.pm/rrobin.glade/
+        or LOGDIE "Could not find the Glade file.\n";
 
     # Now we can load it.
     my $self    = $class->SUPER::new($glade_file);
+    DEBUG 'Created a new instance of Gtk2::GladeXML::Simple.';
 
     return $self;
 }
@@ -72,6 +86,26 @@ When someone clicks on the little cross... then quit.
 
 =cut
 
-sub mainwindow_quit { Gtk2->main_quit }
+sub mainwindow_quit { 
+    _log_call;
+    
+    Gtk2->main_quit;
+}
+
+sub new_clicked {
+    _log_call;
+}
+
+
+=head1 OTHER SUBROUTINES
+
+=head2 _logcall
+
+This subroutine is just for intern use. It logs the called function name
+when a signal is emitted by the GUI.
+
+=cut
+# It was not possible to place it here because of the documentation
+# structure and because it has to be known earlier.
 
 1;
